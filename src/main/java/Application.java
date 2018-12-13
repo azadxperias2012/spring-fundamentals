d8ed8ca8b48108c50b55fce8b024c23c442e9be7
Azad Asanali <azadxperias2012@gmail.com>
2018-12-13 23:05:10 +0530

    spring scope - singleton demo

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,18 @@
 import com.neotechlabs.service.CustomerService;
-import com.neotechlabs.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        CustomerService service = new CustomerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
+
+        System.out.println(service);
+
+        CustomerService service2 = appContext.getBean("customerService", CustomerService.class);
+        System.out.println(service2);
 
         System.out.println("FirstName: " + service.findAll().get(0).getFirstName());
         System.out.println("LastName: " + service.findAll().get(0).getLastName());
--- src/main/java/com/neotechlabs/service/CustomerServiceImpl.java
+++ src/main/java/com/neotechlabs/service/CustomerServiceImpl.java
@@ -2,17 +2,30 @@ package com.neotechlabs.service;
 
 import com.neotechlabs.model.Customer;
 import com.neotechlabs.repository.CustomerRepository;
-import com.neotechlabs.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl() {
+    }
 
     @Override
     public List<Customer> findAll() {
         return customerRepository.findAll();
     }
 
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("We are using setter injection");
+        this.customerRepository = customerRepository;
+    }
 }
